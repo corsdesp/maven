8ca807ca75b71dd8f97874e95b70a44e5b6313c7
Dmitrii Churovoi <Dmitrii_Churovoi@epam.com>
2017-04-18 18:10:54 +0300

    complete task

---
+++ maven-webapp/src/main/java/com.epam.training.app2/WebApp.java
@@ -0,0 +1,11 @@
+package com.epam.training.app2;
+
+
+import com.epam.training.app.App;
+
+public class WebApp {
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println("!!!");
+    }
+}
